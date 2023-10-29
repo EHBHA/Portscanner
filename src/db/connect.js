e1e52fbd8e9434c4fc504fd01c9f0b8ee4a65c87
EHBHA <149295915+EHBHA@users.noreply.github.com>
2023-10-29 15:22:57 +0530

    Update connect.js

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 mongoose.set('strictQuery',true);
-mongoose.connect('mongodb+srv://bharatrn:veda@portscan.rsqmdjd.mongodb.net/?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://<username>:<password>@portscan.rsqmdjd.mongodb.net/?retryWrites=true&w=majority',{
 }).then(()=>{
     console.log("connected");
 }).catch(()=>{
